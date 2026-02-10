8d819542a1ca5a9ecb38987362b12b76623500fd
El Mansouri Yacine <yacine.elmansouri.etu@univ-lille.fr>
2026-02-10 14:26:51 +0100

    test(webinars): implement full testing strategy (unit, int, e2e) for change-seats

--- src/webinars/use-cases/change-seats.test.ts
+++ src/webinars/use-cases/change-seats.test.ts
@@ -1,13 +1,115 @@
-// Tests unitaires
+import { ChangeSeats } from 'src/webinars/use-cases/change-seats';
+import { InMemoryWebinarRepository } from 'src/webinars/adapters/webinar-repository.in-memory';
+import { Webinar } from 'src/webinars/entities/webinar.entity';
+import { testUser } from 'src/users/tests/user-seeds';
+import { WebinarNotFoundException } from 'src/webinars/exceptions/webinar-not-found';
+import { WebinarNotOrganizerException } from 'src/webinars/exceptions/webinar-not-organizer';
+import { WebinarReduceSeatsException } from 'src/webinars/exceptions/webinar-reduce-seats';
+import { WebinarTooManySeatsException } from 'src/webinars/exceptions/webinar-too-many-seats';
 
+describe('Feature: Change seats', () => {
+  let webinarRepository: InMemoryWebinarRepository;
+  let useCase: ChangeSeats;
 
-describe('Feature : Change seats', () => {
-  // Initialisation de nos tests, boilerplates...
+  const webinar = new Webinar({
+    id: 'webinar-id',
+    organizerId: testUser.alice.props.id,
+    title: 'Webinar title',
+    startDate: new Date('2024-01-01T00:00:00Z'),
+    endDate: new Date('2024-01-01T01:00:00Z'),
+    seats: 100,
+  });
+
+  beforeEach(() => {
+    webinarRepository = new InMemoryWebinarRepository([webinar]);
+    useCase = new ChangeSeats(webinarRepository);
+  });
+
+  // Helpers
+  
+  async function expectWebinarToRemainUnchanged() {
+    const webinar = await webinarRepository.findById('webinar-id');
+    expect(webinar?.props.seats).toEqual(100);
+  }
+
+  async function thenUpdatedWebinarSeatsShouldBe(seats: number) {
+    const updatedWebinar = await webinarRepository.findById('webinar-id');
+    expect(updatedWebinar?.props.seats).toEqual(seats);
+  }
+
+  // Scenarios
+
   describe('Scenario: Happy path', () => {
-    // Code commun à notre scénario : payload...
+    const payload = {
+      user: testUser.alice,
+      webinarId: 'webinar-id',
+      seats: 200,
+    };
+
     it('should change the number of seats for a webinar', async () => {
-     // Vérification de la règle métier, condition testée...
-      throw new Error('Not implemented');
+      await useCase.execute(payload);
+      await thenUpdatedWebinarSeatsShouldBe(200);
     });
   });
+
+  describe('Scenario: webinar does not exist', () => {
+    const payload = {
+      user: testUser.alice,
+      webinarId: 'unknown-webinar-id',
+      seats: 200,
+    };
+
+    it('should fail', async () => {
+      await expect(useCase.execute(payload)).rejects.toThrow(
+        WebinarNotFoundException,
+      );
+      await expectWebinarToRemainUnchanged();
+    });
+  });
+
+  describe('Scenario: update the webinar of someone else', () => {
+    const payload = {
+      user: testUser.bob,
+      webinarId: 'webinar-id',
+      seats: 200,
+    };
+
+    it('should fail', async () => {
+      await expect(useCase.execute(payload)).rejects.toThrow(
+        WebinarNotOrganizerException,
+      );
+      await expectWebinarToRemainUnchanged();
+    });
+  });
+
+  describe('Scenario: change seat to an inferior number', () => {
+    const payload = {
+      user: testUser.alice,
+      webinarId: 'webinar-id',
+      seats: 50,
+    };
+
+    it('should fail', async () => {
+      await expect(useCase.execute(payload)).rejects.toThrow(
+        WebinarReduceSeatsException,
+      );
+      await expectWebinarToRemainUnchanged();
+    });
+  });
+
+  describe('Scenario: change seat to a number > 1000', () => {
+    const payload = {
+      user: testUser.alice,
+      webinarId: 'webinar-id',
+      seats: 1001,
+    };
+
+    it('should fail', async () => {
+      await expect(useCase.execute(payload)).rejects.toThrow(
+        WebinarTooManySeatsException,
+      );
+      await expectWebinarToRemainUnchanged();
+    });
+  });
+
 });
